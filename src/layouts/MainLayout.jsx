14cce56c2a8563193833afe217763b715681057b
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-06-11 16:27:58 -0500

    feat: implementando el layout principal

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Sidebar from "../components/sidebar";
+
+export default function MainLayout() {
+  return (
+    <div className="h-screen flex bg-slate-100">
+      <Sidebar/>
+
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header/>
+
+        <main className="flex-1 overflow-y-auto p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
