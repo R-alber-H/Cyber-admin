6e42619beef557fab1d89796462ae3a6cedba9cc
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-06-28 15:51:19 -0500

    agregando userService para obtenr y actualizar usaurios

---
+++ src/services/userService.js
@@ -0,0 +1,21 @@
+import api from "./Api"
+
+export async function getUser() {
+    try {
+        const response = await api.get("/users");
+        return response.data
+    } catch (error) {
+        console.error("Error:", error.response?.data || error.message);
+        throw error;
+    }
+}
+
+export async function updateUser(id,data) {
+    try {
+        const response = await api.put(`/users/${id}`, data)
+        return response.data
+    } catch (error) {
+        console.error("Error:", error.response?.data || error.message);
+        throw error;
+    }
+}
