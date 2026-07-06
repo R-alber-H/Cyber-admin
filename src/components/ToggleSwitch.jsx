7699dfdbdb13341ae1bacf69228f335bac5c4959
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-07-06 14:34:27 -0500

    Agregando avatar y dando estilo al sidebar

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -4,7 +4,7 @@ export function ToggleSwitch({ estado, onToggleState }) {
       onClick={onToggleState}
       className={`w-11 h-6 flex items-center rounded-full p-1 transition-all duration-300 ${
         estado 
-          ? 'bg-violet-600 shadow-sm shadow-violet-200' 
+          ? 'bg-[#162D56] shadow-sm shadow-violet-200' 
           : 'bg-zinc-300 hover:bg-zinc-400/70'
       }`}
     >
